7ae223b4bc4569be17c91c4d2d89f03763defc71
Hirish Chandrasekaran <hirish@ucsb.edu>
2022-11-18 20:13:04 -0600

    added login/sign-up UI

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,11 +1,57 @@
 import React from 'react'
+import { Container, Box, Text,Tabs,TabList,Tab,TabPanel,TabPanels} from '@chakra-ui/react'
+import SignUp from './SignUp';
+import Login from './Login';
 
 const Homepage = () => {
-  return (
-    <div>
-      Homestyle
-    </div>
-  )
-}
+  return <Container maxW='xl' centerContent>
+    <Box
+    d='flex'
+    justifyContent='flex-start'
+    textAlign="center"
+    //bgGradient="linear(to-t, orange.200, pink.500)"
+    bgGradient='linear(to-l, #7928CA, #FF0080)'
+    p={8}
+    opacity="0.9"
+    w="100%"
+    m = "20px 0 15px 0"
+    borderRadius="xl"
+    >
+      <Text color="white" fontSize='8xl'noOfLines={1}
+      >Hazel
+
+      </Text>
+    </Box>
+
+    <Box
+    d='flex'
+    justifyContent='flex-start'
+    textAlign="center"
+    bgGradient="linear(to-t, orange.200, pink.500)"
+    //bgGradient="linear(to-t, #FF0080, pink.500)"
+    opacity="0.9"
+    p={3}
+    w="100%"
+    m = "15px 0 15px 0"
+    borderRadius="xl"
+    >
+    <Tabs variant='soft-rounded'  isFitted >
+      <TabList>
+        <Tab color="white" _selected={{  bg: 'blue.500' }}>Login</Tab>
+        <Tab color="white" _selected={{ bg: 'blue.500' }}>Sign Up</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel>
+          <Login/>
+        </TabPanel>
+        <TabPanel>
+          <SignUp/>
+        </TabPanel>
+      </TabPanels>
+      </Tabs>
+    </Box>
+    
+  </Container>;
+};
 
 export default Homepage
---
+++ frontend/src/Pages/Login.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import { Button, VStack, FormControl, Input, FormLabel, InputGroup, InputRightElement } from '@chakra-ui/react'
+import { useState } from 'react'
+
+
+const Login = () => {
+    const [email,setEmail] = useState();
+    const [password,setPassword] = useState();
+    const [show,setShow] = useState();
+
+    const handleClick = ()=>setShow(!show);
+    const submitHandler = ()=>{};
+
+    return (
+    <VStack spacing="5px">
+
+        <FormControl id="email_login" isRequired>
+            <FormLabel>Email</FormLabel>
+            <Input placeholder='Enter Your Email'
+            onChange={(e)=>setEmail(e.target.value)}
+            >
+            </Input>
+        </FormControl> 
+
+        <FormControl id="password_login" isRequired>
+                <FormLabel>Password</FormLabel>
+                <InputGroup>
+            <Input 
+            type={show ? "text" : "password"}
+            placeholder='Enter Your Password'
+            onChange={(e)=>setPassword(e.target.value)}
+            >
+            </Input>
+            <InputRightElement width="4.5rem">
+                <Button h="1.75rem" size="sm" onClick={handleClick}>
+                    {show ? "Hide" : "Show"}
+                </Button>
+            </InputRightElement>
+            </InputGroup>
+        </FormControl>  
+
+        
+
+        <Button
+            colorScheme="blue"
+            width="100%"
+            style={{marginTop: 15}}
+            onClick={submitHandler}
+            >
+               Log In
+            </Button>
+
+        <Button
+        colorScheme="red"
+        width="100%"
+        style={{marginTop: 15}}
+        onClick={()=>{
+          setEmail("guest@ucsb.edu");
+          setPassword("ucsb");
+        }}
+        >
+            Guest Log In
+        </Button>
+    </VStack>
+    );
+};
+
+export default Login
