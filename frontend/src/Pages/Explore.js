7272efb195e1bbfe760aea28cb9777050fd97c4c
connor11son <clevenson@ucsb.edu>
2022-12-22 00:06:42 -0800

    Added basic code for explore page

--- frontend/src/Pages/Explore.js
+++ frontend/src/Pages/Explore.js
@@ -1,113 +1,465 @@
-import React from 'react'
-import { Container, Box, Text,Tabs,TabList,Tab,TabPanel,TabPanels, Image, Badge, StarIcon} from '@chakra-ui/react'
+import * as React from "react";
+import * as ReactDOMClient from "react-dom/client";
+import { Box, ChakraProvider } from "@chakra-ui/react";
+import {
+  Container,
+  Grid,
+  GridItem,
+  Text,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanel,
+  TabPanels,
+  Image,
+  Badge,
+  StarIcon,
+  Link
+} from "@chakra-ui/react";
+import {
+  Heading,
+  Avatar,
+  Center,
+  Flex,
+  Stack,
+  Button,
+  useColorModeValue
+} from "@chakra-ui/react";
 
-
-
-const Explore = () => {
-
-    const property = {
-    imageUrl: 'https://bit.ly/2Z4KKcF',
-    imageAlt: 'Rear view of modern home with pool',
+function App() {
+  const property = {
+    imageUrl: "https://bit.ly/2Z4KKcF",
+    imageAlt: "Rear view of home with pool",
     beds: 3,
     baths: 2,
-    title: 'Modern home in city center in the heart of historic Los Angeles',
-    formattedPrice: '$1,900.00',
+    title: "Modern home in city in the heart of historic Los Angeles",
+    formattedPrice: "$1,900.00",
     reviewCount: 34,
-    rating: 4,
-    }
-
-
-
+    rating: 4
+  };
   return (
-    <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden' justifyContent='center'>
-      <Image src={property.imageUrl} alt={property.imageAlt} />
-      <Box p='6'>
-        <Box display='flex' alignItems='baseline'>
-          <Badge borderRadius='full' px='2' colorScheme='teal'>
-            New
-          </Badge>
-          <Box
-            color='gray.500'
-            fontWeight='semibold'
-            letterSpacing='wide'
-            fontSize='xs'
-            textTransform='uppercase'
-            ml='2'
-          >
-            {property.beds} beds &bull; {property.baths} baths
-          </Box>
-        </Box>
-
-        <Box
-          mt='1'
-          fontWeight='semibold'
-          as='h4'
-          lineHeight='tight'
-          noOfLines={1}
+    <Grid templateColumns="repeat(5, 4fr)" rowGap={100} columnGap={3}>
+      <Box w="100%" h="10px">
+        <Stack
+          borderWidth="1px"
+          borderRadius="lg"
+          w={{ sm: "100%", md: "540px" }}
+          height={{ sm: "476px", md: "20rem" }}
+          direction={{ base: "column", md: "row" }}
+          bg={useColorModeValue("white", "gray.900")}
+          boxShadow={"2xl"}
+          padding={4}
         >
-          {property.title}
-        </Box>
-
-        <Box>
-          {property.formattedPrice}
-          <Box as='span' color='gray.600' fontSize='sm'>
-            / wk
-          </Box>
-        </Box>
-
-        <Box display='flex' mt='2' alignItems='center'>
-          <Box as='span' ml='2' color='gray.600' fontSize='sm'>
-            {property.reviewCount} reviews
-          </Box>
-        </Box>
-      </Box>
-
-      <Image src={property.imageUrl} alt={property.imageAlt} />
-      <Box p='6'>
-        <Box display='flex' alignItems='baseline'>
-          <Badge borderRadius='full' px='2' colorScheme='teal'>
-            New
-          </Badge>
-          <Box
-            color='gray.500'
-            fontWeight='semibold'
-            letterSpacing='wide'
-            fontSize='xs'
-            textTransform='uppercase'
-            ml='2'
+          <Flex flex={1} bg="blue.200">
+            <Image
+              objectFit="cover"
+              boxSize="100%"
+              src={
+                "https://images.unsplash.com/photo-1520810627419-35e362c5dc07?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ"
+              }
+            />
+          </Flex>
+          <Stack
+            flex={1}
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            p={1}
+            pt={2}
           >
-            {property.beds} beds &bull; {property.baths} baths
-          </Box>
-        </Box>
+            <Heading fontSize={"2xl"} fontFamily={"body"}>
+              Lindsey James
+            </Heading>
+            <Text fontWeight={600} color={"gray.500"} size="sm" mb={4}>
+              @lindsey_jam3s
+            </Text>
+            <Text
+              textAlign={"center"}
+              color={useColorModeValue("gray.700", "gray.400")}
+              px={3}
+            >
+              Actress, musician, songwriter and artist. PM for work inquires or
+              <Link href={"#"} color={"blue.400"}>
+                #tag
+              </Link>
+              me in your posts
+            </Text>
+            <Stack align={"center"} justify={"center"} direction={"row"} mt={6}>
+              <Badge
+                px={2}
+                py={1}
+                bg={useColorModeValue("gray.50", "gray.800")}
+                fontWeight={"400"}
+              >
+                #art
+              </Badge>
+              <Badge
+                px={2}
+                py={1}
+                bg={useColorModeValue("gray.50", "gray.800")}
+                fontWeight={"400"}
+              >
+                #photography
+              </Badge>
+              <Badge
+                px={2}
+                py={1}
+                bg={useColorModeValue("gray.50", "gray.800")}
+                fontWeight={"400"}
+              >
+                #music
+              </Badge>
+            </Stack>
 
-        <Box
-          mt='1'
-          fontWeight='semibold'
-          as='h4'
-          lineHeight='tight'
-          noOfLines={1}
-        >
-          {property.title}
-        </Box>
-
-        <Box>
-          {property.formattedPrice}
-          <Box as='span' color='gray.600' fontSize='sm'>
-            / wk
-          </Box>
-        </Box>
-
-        <Box display='flex' mt='2' alignItems='center'>
-          <Box as='span' ml='2' color='gray.600' fontSize='sm'>
-            {property.reviewCount} reviews
-          </Box>
-        </Box>
+            <Stack
+              width={"100%"}
+              mt={"2rem"}
+              direction={"row"}
+              padding={2}
+              justifyContent={"space-between"}
+              alignItems={"center"}
+            >
+              <Button
+                flex={1}
+                fontSize={"sm"}
+                rounded={"full"}
+                _focus={{
+                  bg: "gray.200"
+                }}
+              >
+                Message
+              </Button>
+              <Button
+                flex={1}
+                fontSize={"sm"}
+                rounded={"full"}
+                bg={"blue.400"}
+                color={"white"}
+                boxShadow={
+                  "0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)"
+                }
+                _hover={{
+                  bg: "blue.500"
+                }}
+                _focus={{
+                  bg: "blue.500"
+                }}
+              >
+                Follow
+              </Button>
+            </Stack>
+          </Stack>
+        </Stack>
       </Box>
-    </Box>
+      <Box w="100%" h="10px">
+        <Stack
+          borderWidth="1px"
+          borderRadius="lg"
+          w={{ sm: "100%", md: "540px" }}
+          height={{ sm: "476px", md: "20rem" }}
+          direction={{ base: "column", md: "row" }}
+          bg={useColorModeValue("white", "gray.900")}
+          boxShadow={"2xl"}
+          padding={4}
+        >
+          <Flex flex={1} bg="blue.200">
+            <Image
+              objectFit="cover"
+              boxSize="100%"
+              src={
+                "https://images.unsplash.com/photo-1520810627419-35e362c5dc07?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ"
+              }
+            />
+          </Flex>
+          <Stack
+            flex={1}
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            p={1}
+            pt={2}
+          >
+            <Heading fontSize={"2xl"} fontFamily={"body"}>
+              Lindsey James
+            </Heading>
+            <Text fontWeight={600} color={"gray.500"} size="sm" mb={4}>
+              @lindsey_jam3s
+            </Text>
+            <Text
+              textAlign={"center"}
+              color={useColorModeValue("gray.700", "gray.400")}
+              px={3}
+            >
+              Actress, musician, songwriter and artist. PM for work inquires or
+              <Link href={"#"} color={"blue.400"}>
+                #tag
+              </Link>
+              me in your posts
+            </Text>
+            <Stack align={"center"} justify={"center"} direction={"row"} mt={6}>
+              <Badge
+                px={2}
+                py={1}
+                bg={useColorModeValue("gray.50", "gray.800")}
+                fontWeight={"400"}
+              >
+                #art
+              </Badge>
+              <Badge
+                px={2}
+                py={1}
+                bg={useColorModeValue("gray.50", "gray.800")}
+                fontWeight={"400"}
+              >
+                #photography
+              </Badge>
+              <Badge
+                px={2}
+                py={1}
+                bg={useColorModeValue("gray.50", "gray.800")}
+                fontWeight={"400"}
+              >
+                #music
+              </Badge>
+            </Stack>
 
-    
-    
-  )
+            <Stack
+              width={"100%"}
+              mt={"2rem"}
+              direction={"row"}
+              padding={2}
+              justifyContent={"space-between"}
+              alignItems={"center"}
+            >
+              <Button
+                flex={1}
+                fontSize={"sm"}
+                rounded={"full"}
+                _focus={{
+                  bg: "gray.200"
+                }}
+              >
+                Message
+              </Button>
+              <Button
+                flex={1}
+                fontSize={"sm"}
+                rounded={"full"}
+                bg={"blue.400"}
+                color={"white"}
+                boxShadow={
+                  "0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)"
+                }
+                _hover={{
+                  bg: "blue.500"
+                }}
+                _focus={{
+                  bg: "blue.500"
+                }}
+              >
+                Follow
+              </Button>
+            </Stack>
+          </Stack>
+        </Stack>
+      </Box>
+      <Box w="100%" h="10px">
+        <Stack
+          borderWidth="1px"
+          borderRadius="lg"
+          w={{ sm: "100%", md: "540px" }}
+          height={{ sm: "476px", md: "20rem" }}
+          direction={{ base: "column", md: "row" }}
+          bg={useColorModeValue("white", "gray.900")}
+          boxShadow={"2xl"}
+          padding={4}
+        >
+          <Flex flex={1} bg="blue.200">
+            <Image
+              objectFit="cover"
+              boxSize="100%"
+              src={
+                "https://images.unsplash.com/photo-1520810627419-35e362c5dc07?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ"
+              }
+            />
+          </Flex>
+          <Stack
+            flex={1}
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            p={1}
+            pt={2}
+          >
+            <Heading fontSize={"2xl"} fontFamily={"body"}>
+              Lindsey James
+            </Heading>
+            <Text fontWeight={600} color={"gray.500"} size="sm" mb={4}>
+              @lindsey_jam3s
+            </Text>
+            <Text
+              textAlign={"center"}
+              color={useColorModeValue("gray.700", "gray.400")}
+              px={3}
+            >
+              Actress, musician, songwriter and artist. PM for work inquires or
+              <Link href={"#"} color={"blue.400"}>
+                #tag
+              </Link>
+              me in your posts
+            </Text>
+            <Stack align={"center"} justify={"center"} direction={"row"} mt={6}>
+              <Badge
+                px={2}
+                py={1}
+                bg={useColorModeValue("gray.50", "gray.800")}
+                fontWeight={"400"}
+              >
+                #art
+              </Badge>
+              <Badge
+                px={2}
+                py={1}
+                bg={useColorModeValue("gray.50", "gray.800")}
+                fontWeight={"400"}
+              >
+                #photography
+              </Badge>
+              <Badge
+                px={2}
+                py={1}
+                bg={useColorModeValue("gray.50", "gray.800")}
+                fontWeight={"400"}
+              >
+                #music
+              </Badge>
+            </Stack>
+
+            <Stack
+              width={"100%"}
+              mt={"2rem"}
+              direction={"row"}
+              padding={2}
+              justifyContent={"space-between"}
+              alignItems={"center"}
+            >
+              <Button
+                flex={1}
+                fontSize={"sm"}
+                rounded={"full"}
+                _focus={{
+                  bg: "gray.200"
+                }}
+              >
+                Message
+              </Button>
+              <Button
+                flex={1}
+                fontSize={"sm"}
+                rounded={"full"}
+                bg={"blue.400"}
+                color={"white"}
+                boxShadow={
+                  "0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)"
+                }
+                _hover={{
+                  bg: "blue.500"
+                }}
+                _focus={{
+                  bg: "blue.500"
+                }}
+              >
+                Follow
+              </Button>
+            </Stack>
+          </Stack>
+        </Stack>
+      </Box>
+    </Grid>
+  );
 }
 
-export default Explore
+const rootElement = document.getElementById("root");
+const root = ReactDOMClient.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
+
+export default function SocialProfileWithImage() {
+  return (
+    <Center py={6}>
+      <Box
+        maxW={"270px"}
+        w={"full"}
+        bg={useColorModeValue("white", "gray.800")}
+        boxShadow={"2xl"}
+        rounded={"md"}
+        overflow={"hidden"}
+      >
+        <Image
+          h={"120px"}
+          w={"full"}
+          src={
+            "https://images.unsplash.com/photo-1612865547334-09cb8cb455da?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+          }
+          objectFit={"cover"}
+        />
+        <Flex justify={"center"} mt={-12}>
+          <Avatar
+            size={"xl"}
+            src={
+              "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ"
+            }
+            alt={"Author"}
+            css={{
+              border: "2px solid white"
+            }}
+          />
+        </Flex>
+
+        <Box p={6}>
+          <Stack spacing={0} align={"center"} mb={5}>
+            <Heading fontSize={"2xl"} fontWeight={500} fontFamily={"body"}>
+              John Doe
+            </Heading>
+            <Text color={"gray.500"}>Frontend Developer</Text>
+          </Stack>
+
+          <Stack direction={"row"} justify={"center"} spacing={6}>
+            <Stack spacing={0} align={"center"}>
+              <Text fontWeight={600}>23k</Text>
+              <Text fontSize={"sm"} color={"gray.500"}>
+                Followers
+              </Text>
+            </Stack>
+            <Stack spacing={0} align={"center"}>
+              <Text fontWeight={600}>23k</Text>
+              <Text fontSize={"sm"} color={"gray.500"}>
+                Followers
+              </Text>
+            </Stack>
+          </Stack>
+
+          <Button
+            w={"full"}
+            mt={8}
+            bg={useColorModeValue("#151f21", "gray.900")}
+            color={"white"}
+            rounded={"md"}
+            _hover={{
+              transform: "translateY(-2px)",
+              boxShadow: "lg"
+            }}
+          >
+            Follow
+          </Button>
+        </Box>
+      </Box>
+    </Center>
+  );
+}
